68e2bfdb651d26f0548c7c557198361a1b0f0805
Drovwosek <61798378+Drovwosek@users.noreply.github.com>
2025-03-20 05:07:06 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', 
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
